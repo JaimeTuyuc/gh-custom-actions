f6c2f34f512f012a1cfda8fdf506a303d1795fb1
Jaime Tuyuc <jaime.tuyuc@telus.com>
2023-12-23 18:13:36 -0600

    adding custom actions

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,21 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-  core.notice('Hello from JavaScript action');
+  //! Get the inputs from the workflow file:
+
+  // Get the bucket name
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: false });
+  
+  //! Upload files to S3 bucket
+  exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+  // exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion} --delete`);
+
+  const webisteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+
+  //! Output the website url
+  core.setOutput('website-url', webisteUrl);
 }
 
 run();
